ad07537d20c7f4bfe4196e764c139ca3faf5bf24
query-gani <ahmadxgani01@gmail.com>
2022-10-23 18:33:02 +0700

    refactor: delete unused file, refactor UX

--- src/components/templates/Main.tsx
+++ src/components/templates/Main.tsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 function Main() {
   return (
     <main>
-      <Link to="/post/26be68a2-ec55-4fc7-bcae-3eb8d89929e1" className="flex w-[982px] h-[307px] justify-between p-5 box-content mx-auto border-b">
+      <div className="flex w-[982px] h-[307px] justify-between p-5 box-content mx-auto border-b">
         <div className=" w-3/4 flex flex-col justify-between">
           <div className="flex gap-2 flex-col">
             <div className="flex items-center gap-2">
@@ -12,13 +12,13 @@ function Main() {
                 Shinigami Zero<span className="mx-1">&#x2219;</span>Jul 13
               </p>
             </div>
-            <div>
+            <Link to="/post/26be68a2-ec55-4fc7-bcae-3eb8d89929e1">
               <h2 className="font-bold text-xl">Title Lorem Ipsum</h2>
               <p>
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam quis enim numquam tempore totam, rerum est, veniam quam eligendi error fugiat blanditiis, doloribus nihil? Qui possimus natus facilis esse ea. Lorem ipsum dolor
                 sit amet consectetur adipisicing elit. Iure non ab quia velit adipisci neque dignissimos autem? Ad neque quas aut minima, obcaecati aspernatur, molestias at molestiae perferendis nihil qui.
               </p>
-            </div>
+            </Link>
           </div>
           <div className="flex justify-between">
             <div className="flex gap-2 items-center">
@@ -31,10 +31,10 @@ function Main() {
             </div>
           </div>
         </div>
-        <div className="w-[235px] h-[195px] bg-slate-500 flex items-center justify-center rounded-[30px] self-center">
+        <Link to="/post/26be68a2-ec55-4fc7-bcae-3eb8d89929e1" className="w-[235px] h-[195px] bg-slate-500 flex items-center justify-center rounded-[30px] self-center">
           <h2 className="font-black">Thumbnail</h2>
-        </div>
-      </Link>
+        </Link>
+      </div>
     </main>
   );
 }
