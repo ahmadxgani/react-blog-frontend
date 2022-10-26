f5167898e5d686c077f7975681e652dbf23fc5e1
query-gani <ahmadxgani01@gmail.com>
2022-10-26 23:12:14 +0700

    feat: update UI

--- src/components/templates/Main.tsx
+++ src/components/templates/Main.tsx
@@ -2,38 +2,32 @@ import { Link } from "react-router-dom";
 
 function Main() {
   return (
-    <main>
-      <div className="flex w-[982px] h-[307px] justify-between p-5 box-content mx-auto border-b">
-        <div className=" w-3/4 flex flex-col justify-between">
+    <main className="bg-[#E6E5F3] h-[100vh] flex flex-col justify-center">
+      <div className="flex w-[982px] h-[307px] justify-between pl-[45px] pr-[50px] py-[60px] box-content mx-auto border-b bg-white rounded-xl">
+        <Link to="/post/26be68a2-ec55-4fc7-bcae-3eb8d89929e1" className="mr-[50px] relative">
+          <img src={process.env.PUBLIC_URL + "/img/example/thumbnail.jpg"} alt="Thumbnail" width={400} height={300} className="rounded-[20px]" />
+          <div className="pt-[25px] pr-[25px] top-0 right-0 absolute">
+            <img src={process.env.PUBLIC_URL + "/img/example/profile.jpg"} alt="Profile" className="border-[5px] border-solid border-white rounded-full w-[50px] h-[50px]" />
+          </div>
+        </Link>
+        <div className="w-3/4 flex flex-col justify-between">
           <div className="flex gap-2 flex-col">
-            <div className="flex items-center gap-2">
-              <div className="rounded-full w-[43px] h-[43px] bg-slate-500" />
-              <p>
-                Shinigami Zero<span className="mx-1">&#x2219;</span>Jul 13
-              </p>
-            </div>
-            <Link to="/post/26be68a2-ec55-4fc7-bcae-3eb8d89929e1">
-              <h2 className="font-bold text-xl">Title Lorem Ipsum</h2>
-              <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam quis enim numquam tempore totam, rerum est, veniam quam eligendi error fugiat blanditiis, doloribus nihil? Qui possimus natus facilis esse ea. Lorem ipsum dolor
-                sit amet consectetur adipisicing elit. Iure non ab quia velit adipisci neque dignissimos autem? Ad neque quas aut minima, obcaecati aspernatur, molestias at molestiae perferendis nihil qui.
-              </p>
+            <Link to="/post/26be68a2-ec55-4fc7-bcae-3eb8d89929e1" className="font-bold text-[40px] text-[#353443]">
+              How to be an Ideal Engineer React JS
+            </Link>
+            <p className="text-xl">
+              <span className="text-[#37A0C1] font-bold">React JS&#9;</span>
+              <span className="text-[#8C8B93]">|&#9;</span>
+              <span className="text-[#8E8D93]">6 min read</span>
+            </p>
+            <Link to="/post/26be68a2-ec55-4fc7-bcae-3eb8d89929e1" className="text-[22px] text-[#57565C]">
+              Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley.
             </Link>
           </div>
-          <div className="flex justify-between">
-            <div className="flex gap-2 items-center">
-              <div className="bg-slate-200 rounded-lg p-1">programming</div>
-              <span>2 menit yang lalu</span>
-            </div>
-            <div className="flex gap-2">
-              <img src={process.env.PUBLIC_URL + "/img/icon/Bookmark.png"} alt="bookmark" width={28} />
-              <span className="font-bold text-lg">&#x2219;&#x2219;&#x2219;</span>
-            </div>
-          </div>
         </div>
-        <Link to="/post/26be68a2-ec55-4fc7-bcae-3eb8d89929e1" className="w-[235px] h-[195px] bg-slate-500 flex items-center justify-center rounded-[30px] self-center">
-          <h2 className="font-black">Thumbnail</h2>
-        </Link>
+        <div>
+          <img src={process.env.PUBLIC_URL + "/img/icon/Bookmark.png"} alt="bookmark" width={45} />
+        </div>
       </div>
     </main>
   );
