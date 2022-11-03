430fccc640b07edd38cdd43975ca3de6be19deed
QUERY-GANI <ahmadxgani01@gmail.com>
2022-11-03 13:29:40 +0700

    refactor

--- src/components/templates/Editor.tsx
+++ src/components/templates/Editor.tsx
@@ -1,5 +1,5 @@
 import "./editor.css";
-import { Fragment, useEffect, useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import EditorJS, { LogLevels, OutputData } from "@editorjs/editorjs";
 import Header from "@editorjs/header";
 
@@ -55,11 +55,7 @@ const Editor = (props: any) => {
     });
   };
 
-  return (
-    <Fragment>
-      <div className="prose lg:prose-lg xl:prose-xl prose-sm bg-white p-3 rounded-lg shadow w-full" id={EDITTOR_HOLDER_ID}></div>
-    </Fragment>
-  );
+  return <div className="prose lg:prose-lg xl:prose-xl prose-sm bg-white p-3 rounded-lg shadow w-full" id={EDITTOR_HOLDER_ID}></div>
 };
 
 export default Editor;
