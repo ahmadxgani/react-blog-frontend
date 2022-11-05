0b419863136e9fcdb5bcf31c048645d7291d2838
query-gani <ahmadxgani01@gmail.com>
2022-11-05 08:01:16 +0700

    feat: add example

--- src/components/post/ShowAllPost.tsx
+++ src/components/post/ShowAllPost.tsx
@@ -38,6 +38,62 @@ function ShowAllPost() {
         </div>
         <img src={process.env.PUBLIC_URL + "/img/icon/Bookmark.png"} alt="bookmark" className="w-8 h-8" />
       </div>
+
+      <div className="flex p-[1.125rem] gap-[2.1875rem] justify-center bg-white rounded-xl box-content">
+        <div className="flex gap-5">
+          <Link to="/post/26be68a2-ec55-4fc7-bcae-3eb8d89929e1" className="relative min-w-[14.6875rem] max-w-[14.6875rem] sm:block hidden">
+            <img src={process.env.PUBLIC_URL + "/img/example/thumbnail.jpg"} alt="Thumbnail" className="rounded-[1.25rem]" />
+            <div className="pt-[0.625rem] pr-[0.625rem] top-0 right-0 absolute">
+              <img src={process.env.PUBLIC_URL + "/img/example/profile.jpg"} alt="Profile" className="border-[3px] border-solid border-white rounded-full w-8 h-8" />
+            </div>
+          </Link>
+          <div className="max-w-[26.125rem] flex flex-col gap-2">
+            <div>
+              <Link to="/post/26be68a2-ec55-4fc7-bcae-3eb8d89929e1" className="font-bold lg:text-3xl sm:text-2xl text-sm leading-6 text-[#353443]">
+                How to be an Ideal Engineer React JS
+              </Link>
+              <p className="sm:text-xl text-sm">
+                <small className="text-[#37A0C1] font-bold">React JS&#9;</small>
+                <span className="text-[#8C8B93] font-light">|&#9;</span>
+                <small className="text-[#8E8D93]">6 min read</small>
+              </p>
+            </div>
+
+            <Link to="/post/26be68a2-ec55-4fc7-bcae-3eb8d89929e1" className="text-[#57565C] text-sm overflow-hidden">
+              Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s...
+            </Link>
+          </div>
+        </div>
+        <img src={process.env.PUBLIC_URL + "/img/icon/Bookmark.png"} alt="bookmark" className="w-8 h-8" />
+      </div>
+
+      <div className="flex p-[1.125rem] gap-[2.1875rem] justify-center bg-white rounded-xl box-content">
+        <div className="flex gap-5">
+          <Link to="/post/26be68a2-ec55-4fc7-bcae-3eb8d89929e1" className="relative min-w-[14.6875rem] max-w-[14.6875rem] sm:block hidden">
+            <img src={process.env.PUBLIC_URL + "/img/example/thumbnail.jpg"} alt="Thumbnail" className="rounded-[1.25rem]" />
+            <div className="pt-[0.625rem] pr-[0.625rem] top-0 right-0 absolute">
+              <img src={process.env.PUBLIC_URL + "/img/example/profile.jpg"} alt="Profile" className="border-[3px] border-solid border-white rounded-full w-8 h-8" />
+            </div>
+          </Link>
+          <div className="max-w-[26.125rem] flex flex-col gap-2">
+            <div>
+              <Link to="/post/26be68a2-ec55-4fc7-bcae-3eb8d89929e1" className="font-bold lg:text-3xl sm:text-2xl text-sm leading-6 text-[#353443]">
+                How to be an Ideal Engineer React JS
+              </Link>
+              <p className="sm:text-xl text-sm">
+                <small className="text-[#37A0C1] font-bold">React JS&#9;</small>
+                <span className="text-[#8C8B93] font-light">|&#9;</span>
+                <small className="text-[#8E8D93]">6 min read</small>
+              </p>
+            </div>
+
+            <Link to="/post/26be68a2-ec55-4fc7-bcae-3eb8d89929e1" className="text-[#57565C] text-sm overflow-hidden">
+              Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s...
+            </Link>
+          </div>
+        </div>
+        <img src={process.env.PUBLIC_URL + "/img/icon/Bookmark.png"} alt="bookmark" className="w-8 h-8" />
+      </div>
     </div>
   );
 }
