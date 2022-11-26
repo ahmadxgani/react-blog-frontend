2a6c1b4227509b18f14eb8a36e00583bcb1eee2e
query-gani <ahmadxgani01@gmail.com>
2022-11-26 23:55:24 +0700

    feat: add default value on input tag

--- src/components/auth/Profile.tsx
+++ src/components/auth/Profile.tsx
@@ -1,4 +1,21 @@
+import { ChangeEventHandler, useState } from "react";
+import { useUser } from "../../global/UserProvider";
+import { User } from "../../lib/types";
+
 const Profile = () => {
+  const user = useUser();
+  const [username, setUsername] = useState((user?.currentUser.user as User).username);
+  const [image, setImage] = useState();
+
+  const handleUsername: ChangeEventHandler<HTMLInputElement> = (e) => {
+    e.preventDefault();
+    setUsername(e.target.value);
+  };
+
+  const handleImage: ChangeEventHandler<HTMLInputElement> = (e) => {
+    console.dir(e.target.files);
+  };
+
   return (
     <div className="p-5 bg-white rounded-xl w-96 flex flex-col gap-5">
       <div className="flex justify-between">
@@ -9,17 +26,17 @@ const Profile = () => {
         <div className="flex flex-col gap-2">
           <div className="flex flex-col">
             <label htmlFor="username">Username</label>
-            <input className="px-2 focus:outline-none border py-1 rounded" type="text" name="username" id="username" />
+            <input className="px-2 focus:outline-none border py-1 rounded" type="text" name="username" id="username" value={username} onChange={handleUsername} />
           </div>
           <div className="flex flex-col">
             <label htmlFor="email">Email</label>
-            <input className="px-2 focus:outline-none border py-1 rounded" type="email" name="email" id="email" />
+            <input className="px-2 focus:outline-none border py-1 rounded" type="email" name="email" id="email" value={(user?.currentUser.user as User).email} disabled />
           </div>
           <div className="flex flex-col">
             <label htmlFor="profile_image">Profile Image</label>
             <div className="flex gap-2 items-center">
               <img className="rounded-full" src={process.env.PUBLIC_URL + "/img/example/user.jpeg"} alt="user's profile" width={40} />
-              <input type="file" name="profile_image" id="profile_image" />
+              <input type="file" name="profile_image" id="profile_image" onChange={handleImage} />
             </div>
           </div>
         </div>
