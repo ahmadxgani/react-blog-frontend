c3c10b7b1aa481f02d32b27859bede25ab982a85
QUERY-GANI <ahmadxgani01@gmail.com>
2022-11-28 08:14:36 +0700

    feat: use navlink for navigation insted of link react router

--- src/components/admin/Dashboard.tsx
+++ src/components/admin/Dashboard.tsx
@@ -1,15 +1,15 @@
-import { Link, Outlet } from "react-router-dom";
+import { NavLink, Outlet } from "react-router-dom";
 
 export default function Dashboard() {
   return (
     <>
-      <div className="flex flex-col gap-3 p-4 py-6 bg-white rounded-full absolute right-10">
-        <Link to="/dashboard/users">
+      <div className="flex flex-col items-center gap-3 p-3 py-6 bg-white rounded-full absolute right-10">
+        <NavLink to="/dashboard/users" className={({ isActive }) => (isActive ? "bg-[#E6E5F3] rounded p-1 px-2" : undefined)}>
           <img src={process.env.PUBLIC_URL + "/img/icon/ManageUser.png"} alt="manage user" width={30} />
-        </Link>
-        <Link to="/dashboard/tags">
+        </NavLink>
+        <NavLink to="/dashboard/tags" className={({ isActive }) => (isActive ? "bg-[#E6E5F3] rounded p-1 px-2" : undefined)}>
           <img src={process.env.PUBLIC_URL + "/img/icon/Tags.png"} alt="manage tags" width={30} />
-        </Link>
+        </NavLink>
       </div>
       <Outlet />
     </>
